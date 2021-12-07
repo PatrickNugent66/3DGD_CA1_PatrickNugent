805e5cacd05e390de764542612e719e949b52a00
PatrickNugent66 <D00218208@student.dkit.ie>
2021-12-07 12:21:57 +0000

    Add scripts and input system
    -Imported package for the input system that will be used for player movement.
    -Imported scripts from the class codebase that might be useful.
    -Began setting up the player character by adding the model in with relevant scripts.

---
+++ Assets/Scripts/Common/Scripts/Controllers/Door/DoorToggleEventTrigger.cs
@@ -0,0 +1,28 @@
+﻿using GD.Events;
+using UnityEngine;
+
+//public enum TriggerEntity : sbyte
+//{
+//    Player, NONPC, Robot
+//}
+
+public class DoorToggleEventTrigger : MonoBehaviour
+{
+    public string targetTag;
+    public GameEvent OnDoorToggle;
+
+    //  public TriggerEntity entityTag;
+
+    private void Awake()
+    {
+        targetTag = targetTag.Trim();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(targetTag))
+        {
+            OnDoorToggle?.Raise();
+        }
+    }
+}
---
+++ Assets/Scripts/Common/Scripts/Controllers/Door/FSM/FSMDoorController.cs
@@ -0,0 +1,76 @@
+﻿using GD.State;
+using System;
+using UnityEngine;
+
+public class FSMDoorController : MonoBehaviour
+{
+    //accessible to the inspector and IStates
+    [Tooltip("Instance of DoorTranslationParameters used to open/close door")]
+    public DoorTranslationParameters movementParameters;
+
+    //accessible to the IStates
+    [HideInInspector]
+    public float currentDisplacement = 0;
+
+    //accessible to the IStates
+    [HideInInspector]
+    public Vector3 displacedPosition;
+
+    //accessible to the IStates
+    [HideInInspector]
+    public bool IsToggleRequestReceived;
+
+    //hidden from inspector and IStates
+    private StateMachine stateMachine;
+
+    private void Awake()
+    {
+        stateMachine = new StateMachine();
+
+        var closed = new StandClosed(this);
+        var opening = new OpeningDoor(this);
+        var opened = new StandOpen(this);
+        var closing = new ClosingDoor(this);
+
+        stateMachine.AddTransition(closed, opening, HasReceivedOpenRequest());
+
+        stateMachine.AddTransition(opening, opened, HasFullyDisplaced());
+        stateMachine.AddTransition(opened, closing, HasReceivedCloseRequest());
+        stateMachine.AddTransition(closing, closed, HasFullyDisplaced());
+
+        Func<bool> HasReceivedOpenRequest() => () => IsToggleRequestReceived;
+        Func<bool> HasReceivedCloseRequest() => () => IsToggleRequestReceived;
+        Func<bool> HasFullyDisplaced() => () => currentDisplacement >= movementParameters.maximum;
+
+        //     Func<bool> IsSatisfied() => () => prices == bAffordable && menu.Supports("GF");
+
+        //Func<int, int, int> arithmeticFunc = add;
+        //Func<int, int, double> func2 = (x, y) => { return x / y; };
+
+        stateMachine.SetState(closed);
+    }
+
+    //public void ProcessNumbers(List<int> a, List<int> b, Func<int, int, int> arithmetic)
+    //{
+    //    int sum;
+    //    for (int i = 0; i < a.Count; i++)
+    //    {
+    //        sum += arithmetic(a[i], b[i]);
+    //    }
+    //}
+
+    //public int add(int a, int b)
+    //{
+    //    return a + b;
+    //}
+
+    private void Update()
+    {
+        stateMachine.Update();
+    }
+
+    public void HandleDoorToggle()
+    {
+        IsToggleRequestReceived = true;
+    }
+}
