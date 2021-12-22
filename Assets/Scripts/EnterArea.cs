266e1707d33314a4a62063c33db300638fcece76
PatrickNugent66 <D00218208@student.dkit.ie>
2021-12-22 18:46:05 +0000

    Add ability to enter/exit apartment
    -Added an enter an exit trigger into the street and apartments scenes.
    -Created a script called EnterArea that sends the player to the apartment or street area depending on which trigger they enter.

---
+++ Assets/Scripts/EnterArea.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnterArea : MonoBehaviour
+{
+    /// <summary>
+    /// Sends the player object inside/outside the
+    /// apartment depending on which trigger was entered
+    /// once the player enters the trigger
+    /// </summary>
+    /// <param name="other">
+    /// the player object
+    /// </param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player") && gameObject.tag.Equals("EnterTrigger"))
+        {
+            //Temporarily disable any components that prevent 
+            //changing the player's position
+            other.gameObject.GetComponent<BoxCollider>().enabled = false;
+            other.gameObject.GetComponent<NavMeshAgent>().enabled = false;
+
+            other.transform.position = new Vector3(104.283f, 0.029f, 90.529f);
+
+            StartCoroutine(EnableComponents(other.gameObject));
+        }
+        else if(other.gameObject.tag.Equals("Player") && gameObject.tag.Equals("ExitTrigger"))
+        {
+            other.gameObject.GetComponent<BoxCollider>().enabled = false;
+            other.gameObject.GetComponent<NavMeshAgent>().enabled = false;
+
+            other.transform.position = new Vector3(105.269f, 0.249f, 76.661f);
+
+            StartCoroutine(EnableComponents(other.gameObject));
+        }
+    }
+
+    /// <summary>
+    /// Enables the player's components again for
+    /// handling collisions and movement
+    /// </summary>
+    /// <param name="player">
+    /// the player object
+    /// </param>
+    protected IEnumerator EnableComponents(GameObject player)
+    {
+        yield return new WaitForSeconds(0.5f);
+        player.GetComponent<BoxCollider>().enabled = true;
+        player.GetComponent<NavMeshAgent>().enabled = true;
+    }
+}
