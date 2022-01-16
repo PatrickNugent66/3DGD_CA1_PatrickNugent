3c3d93026ab629549f9c1432f9684ae69429873d
PatrickNugent66 <D00218208@student.dkit.ie>
2022-01-16 17:08:23 +0000

    Finish distraction scenario
    -Added animations to the homeless man and cop controllers to be used when playing out the distraction scenario.
    -Added parameters to the homeless man and cop controllers to track whether the characters are walking, etc..
    -Fixed an issue where the player character would rotate after colliding with an object.
    -Modified some conversations to track quests more correctly.

--- Assets/Scripts/CopDistracted.cs
+++ Assets/Scripts/CopDistracted.cs
@@ -10,11 +10,27 @@ public class CopDistracted : MonoBehaviour
 
     private NavMeshAgent agent;
 
+    private Animator anim;
+
+    private bool finished = false;
+
     void Start()
     {
+        anim = this.GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         agent.autoBraking = true;
 
+        anim.SetBool("IsWalking", true);
+
         agent.destination = destinationPoint.position;
+    }
+
+    void Update()
+    {
+        if (agent.remainingDistance < 0.1f && !finished)
+        {
+            finished = true;
+            anim.SetBool("IsWalking", false);
+        }
     }
 }
--- Assets/Scripts/Distraction.cs
+++ Assets/Scripts/Distraction.cs
@@ -1,27 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PixelCrushers.DialogueSystem;
 
 public class Distraction : MonoBehaviour
 {
     [SerializeField]
     private Transform[] destinationPoints;
 
+    [SerializeField]
+    private GameObject cop;
+
+    private Animator anim;
+
     private int currentPoint = 0;
 
     private UnityEngine.AI.NavMeshAgent agent;
 
+    private bool finished = false;
+
     void Start()
     {
+        anim = this.GetComponent<Animator>();
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         agent.autoBraking = true;
+
+        //Remove Usable components from both characters to prevent further dialogue
+        this.GetComponent<Usable>().enabled = false;
+        cop.GetComponent<Usable>().enabled = false;
+
+        anim.SetBool("IsWalking", true);
 
         HandleMovement();
     }
 
     void Update()
     {
-        if (agent.remainingDistance < 0.1f)
+        //If the character is near the next point and not finished then handle movement
+        if (agent.remainingDistance < 0.1f && !finished)
         {
             HandleMovement();
         }
@@ -29,10 +45,19 @@ public class Distraction : MonoBehaviour
 
     void HandleMovement()
     {
-        if (destinationPoints.Length != currentPoint)
+        //If the character is at the last point then able the cop's script, otherwise set
+        //the current point index to the next point.
+        if (currentPoint != destinationPoints.Length)
         {
-            currentPoint++;
             agent.destination = destinationPoints[currentPoint].position;
+            currentPoint++;
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+            anim.SetBool("IsKicking", true);
+            finished = true;
+            cop.GetComponent<CopDistracted>().enabled = true;
         }
     }
 }
